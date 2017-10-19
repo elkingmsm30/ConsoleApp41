79a8fe8922ba064e2ca36db78198d1eab348b696
elkingmsm30 <elkingmsm30@hotmail.com>
2017-10-19 06:45:31 +0200

    hhh

---
+++ ConsoleApp41/ram.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp41
+{
+    class ram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Helllo World");
+            Console.ReadKey();
+
+        }
+    }
+}
